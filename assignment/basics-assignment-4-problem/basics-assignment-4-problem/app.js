a999d9e8e52b96b7c66676e559f75b0aba1fc58c
Egi Pebriyawan <egipebriyawan02@gmail.com>
2025-06-13 00:36:56 +0700

    menambah fitur styling css dynamic ke background

---
+++ assignment/basics-assignment-4-problem/basics-assignment-4-problem/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  //
+  data: function () {
+    return {
+      // isi data disini
+      className: "",
+    };
+  },
+
+  methods: {},
+
+  computed: {
+    // Define your computed property as a function
+  },
+
+  watch: {},
+});
+
+app.mount("#assignment");
