072e3adb5a95f23bb60a4f06af250f969315bf5f
Egi Pebriyawan <egipebriyawan02@gmail.com>
2025-06-25 23:52:44 +0700

    Rendering conditional content & lists

---
+++ section-3-rendering-conditional-content-list/lists-cond-01-starting-setup/app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      enteredGoalValue: "",
+      goals: [],
+    };
+  },
+
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+      this.enteredGoalValue = "";
+    },
+  },
+});
+
+app.mount("#user-goals");
