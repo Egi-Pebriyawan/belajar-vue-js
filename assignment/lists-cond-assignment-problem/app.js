30cfb114397e2e5aecf07919e9a0590577fbedbd
Egi Pebriyawan <egipebriyawan02@gmail.com>
2025-07-13 23:46:41 +0700

    merapihkan folder assignment dan mencoba assignment terbaru list-cond-assignment-problem

---
+++ assignment/lists-cond-assignment-problem/app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+  //
+  data: function () {
+    return {
+      // isi data disini
+    };
+  },
+
+  methods: {},
+
+  computed: {
+    // Define your computed property as a function
+  },
+
+  watch: {},
+});
+
+app.mount("#assignment");
