0488f0f5409730c47fd116c50727c83ad4dfa379
Egi Pebriyawan <egipebriyawan02@gmail.com>
2025-07-17 23:29:30 +0700

    Mengerjakan Assignment

--- assignment/lists-cond-assignment-problem/app.js
+++ assignment/lists-cond-assignment-problem/app.js
@@ -3,13 +3,31 @@ const app = Vue.createApp({
   data: function () {
     return {
       // isi data disini
+      newTask: "",
+      tasks: [],
+      showList: true,
     };
   },
 
-  methods: {},
+  methods: {
+    // buat fungsi ketika tombol Add Task di klik
+    addTask() {
+      if (this.newTask.trim() !== "") {
+        this.tasks.push(this.newTask);
+        this.newTask = "";
+      }
+    },
+
+    toggleList() {
+      this.showList = !this.showList;
+    },
+  },
 
   computed: {
     // Define your computed property as a function
+    listButtonLabel() {
+      return this.showList ? "Hide List" : "Show List";
+    },
   },
 
   watch: {},
