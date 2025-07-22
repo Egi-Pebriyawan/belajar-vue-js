472df07310970689b5c9be456ae1a4e50d63ef75
Egi Pebriyawan <egipebriyawan02@gmail.com>
2025-07-23 01:09:59 +0700

    Adding 57. Heal Function 58. Game Over Status 59.Finishing The Core Functionality

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -12,14 +12,21 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      winner: null,
     };
   },
 
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHealth + "%" };
     },
     mayUseSpecialAttack() {
@@ -27,7 +34,37 @@ const app = Vue.createApp({
     },
   },
 
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        //player draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        //player lost
+        this.winner = "monster";
+        console.log(this.winner);
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        // draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        //monster lost
+        this.winner = "player";
+        console.log(this.winner);
+      }
+    },
+  },
+
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+    },
+
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
@@ -44,6 +81,20 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
+    },
+
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(10, 15);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
+    surrender() {
+      this.winner = "monster";
     },
   },
 });
